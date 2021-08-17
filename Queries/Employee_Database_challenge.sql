198b82acbe282d5a352bd5329f64f21de9de5da1
Gabriela Tuma <gabrielatuma@MacBook-Air-3.local>
2021-08-16 20:00:59 -0400

    Adding deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -8,7 +8,7 @@ SELECT
 	
 -- INTO retirement_titles
 FROM employees as e
-LEFT JOIN titles as tl
+INNER JOIN titles as tl
 ON (e.emp_no = tl.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 --     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
@@ -38,3 +38,22 @@ SELECT title, COUNT (emp_no) as title_qty
 FROM unique_titles
 GROUP BY title
 ORDER BY title_qty DESC
+
+
+SELECT DISTINCT ON (emp_no) 
+       e.emp_no,
+       e.first_name,
+       e.last_name,
+       e.birth_date,
+	   de.from_date,
+       de.to_date,
+	   tl.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_employees as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as tl
+ON (e.emp_no = tl.emp_no)
+
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+     AND (de.to_date = '9999-01-01');
